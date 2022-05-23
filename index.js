37117c73eef65a2d5d899c681e0f5a4dd7f404cd
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-22 23:25:04 -0700

    commit tree f0444493ccea5346de1505f959671fb103922762

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// const p = document.getElementById("text");
+// p.textContent = "This is really cool!";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const p = (document.getElementById("text").textContent =
+    "This is really cool!");
+});
